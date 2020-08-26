56b9b43724e739cf6afd48b3ee14394c0dbc1875
Josh Matthews <josh@joshmatthews.net>
2020-08-26 15:21:27 -0400

    Improve name heuristics

--- newcontributors/newcontributors.sh
+++ newcontributors/newcontributors.sh
@@ -4,15 +4,17 @@ cat new.txt | \
 # remove all @TLD stems from email addresses
 sed -E -e 's/@[A-Za-z\.0-9]+//g' | \
 # fix nameless entries like "  <a>: ..."
-sed -E -e 's/^   <([a-z0-9_\.\-\+]+)>:/\1:/' | \
+sed -E -e 's/^   <([a-z0-9_\.\+\-]+)>:/\1:/' | \
 # fix nameless entries like "[:foo]  <a>:"
 sed -E -e 's/^\[:([A-Za-z0-9_]+)\]   </\1 </' | \
+# remove : prefixes
+sed -E -e 's/:([A-Za-z0-9])/\1/g' | \
 # remove [:name]
-sed -E -e 's/\[:([A-Za-z0-9_]+)\]//g' | \
+sed -E -e 's/\[([A-Za-z0-9_\.\/:;,!?\+ \-]+)\]//g' | \
 # remove (:name)
-sed -E -e 's/\(:([A-Za-z0-9_]+)\)//g' | \
+sed -E -e 's/\(([A-Za-z0-9_\.\/:;,!\?\+ \-]+)\)//g' | \
 # remove <a>
-sed -E -e 's/<([a-z0-9_\.\-\+]+)>//' | \
+sed -E -e 's/<([A-Za-z0-9_\.\+\-]+)>//' | \
 # remove excess spaces
 tr -s ' ' | \
 # remove excess spaces 
